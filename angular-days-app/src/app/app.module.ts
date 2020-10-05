5dca524cd5ab284fd8774243b7c21715f671d154
Martina Kraus <martina.kraus@kardex.com>
2020-10-05 23:41:53 +0200

    cleanup

---
+++ angular-days-app/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UiModule } from './ui.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { IvyComponent } from './ivy-components/ivy.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { CatComponent } from './cat/cat.component';
+@NgModule({
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        IvyComponent,
+        HeaderComponent,
+        HomeComponent,
+        CatComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        UiModule,
+    ],
+    providers: [],
+    bootstrap: [ AppComponent ],
+})
+export class AppModule {}
---
+++ angular-days-app/src/app/ivy-components/ivy.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-ivy-components',
+    templateUrl: './ivy.component.html',
+    styleUrls: [ './ivy.component.scss' ],
+})
+export class IvyComponent {
+    constructor() { }
+
+    lazyLoadComponent(): void {
+        // ToDo Exercise 1: implement lazy loading component
+    }
+}
